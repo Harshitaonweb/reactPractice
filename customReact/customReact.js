09b48ebfa4d4e3aadd2f1dd5cd6bbc51ad8884c8
Harshita Raj <harshitaraj@192.168.1.2>
2025-07-14 16:36:38 +0530

    JS L4

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -11,6 +11,7 @@ function customRender(reactElement, container) {
 
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
+
   for (const prop in reactElement.props) {
     if (prop === "children") continue;
     domElement.setAttribute(prop, reactElement.props[prop]);
