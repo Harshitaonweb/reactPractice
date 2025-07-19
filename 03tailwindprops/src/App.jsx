a9a8bf8113d2a5502f7b71f9fd5700f59187f39a
Harshita Raj <harshitaonweb@gmail.com>
2025-07-19 14:59:29 +0530

    HarryPotter css add

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,33 +8,15 @@ function App() {
 
   return (
     <>
-      <h1 className = 'bg-green-400 text-black p-4 rounded-xl' >Tailwind Test</h1>
-        <div className="flex flex-col rounded-xl  p-4"
-        style={{
-          border: '0.88px solid',
-  
-          backdropFilter: 'saturate(180%) blur(14px)',
-          background: ' #ffffff0d',
-        }}
-      >
-        <div>
-          <img
-            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
-            alt="nft-gif"
-            width="400"
-            height="400"
-            className="rounded-xl"
-          />
-        </div>
-        <div className="flex flex-col  rounded-b-xl py-4 ">
-          <div className="flex justify-between">
-            <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
-            <h1 className="font-bold font-RubikBold">Price</h1>
-          </div>
-          <div className="flex  justify-between font-mono">
-            <p>#345</p>
-            <p>0.01</p>
-          </div>
+      <h1 className = 'bg-orange-700 text-red-950 p-4 rounded-xl' >Harry Potter</h1>
+      <div className="w-90 h-60 rounded-xl ">
+        <img
+          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTFnMW9sZWpoNWxlZ29kczkzcGZqYm8xNzlmNGR3MjNpandmdnQ3ciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26BRzozg4TCBXv6QU/giphy.gif"
+          alt="Giphy"
+          className="rounded-t-xl"
+        />
+        <div className="glass py-4 px-5 relative -top-[3.4rem]  rounded-b-xl z-10">
+          <h1 className="font-bold  font-mono  text-xl text-red-950 bg-slate-300 rounded-2xl bg-opacity-45">Dwell the brave at heart</h1>
         </div>
       </div>
     </>
