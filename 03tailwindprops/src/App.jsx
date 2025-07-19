8179ec1eb79f0c5529cb88971752965f95a9fb03
Harshita Raj <harshitaonweb@gmail.com>
2025-07-19 19:54:32 +0530

    props added

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    username : "harshita",
+    age:18
+  }
+
+  let newArr=[1, 2, 3]
 
   return (
     <>
-      <h1 className = 'bg-orange-700 text-red-950 p-4 rounded-xl' >Harry Potter</h1>
+      <h1 className= 'bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      {/* <h1 className = 'bg-orange-700 text-red-950 p-4 rounded-xl' >Harry Potter</h1>
       <div className="w-90 h-60 rounded-xl ">
         <img
           src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTFnMW9sZWpoNWxlZ29kczkzcGZqYm8xNzlmNGR3MjNpandmdnQ3ciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26BRzozg4TCBXv6QU/giphy.gif"
@@ -18,7 +26,9 @@ function App() {
         <div className="glass py-4 px-5 relative -top-[3.4rem]  rounded-b-xl z-10">
           <h1 className="font-bold  font-mono  text-xl text-red-950 bg-slate-300 rounded-2xl bg-opacity-45">Dwell the brave at heart</h1>
         </div>
-      </div>
+      </div> */}
+      <Card channel="cookieandcode" someObj={newArr}/>
+      <Card />
     </>
   )
 }
