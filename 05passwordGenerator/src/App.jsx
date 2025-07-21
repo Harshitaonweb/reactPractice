0a694d7d67ac160fd88822e3e77485b879767252
Harshita Raj <harshitaonweb@gmail.com>
2025-07-21 22:41:01 +0530

    Added Pass Length & Range

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -8,32 +8,47 @@ function App() {
   const [password, setPassword] = useState("");
 
   const passwordGenerator = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
 
     for (let i = 1; i <= array.length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
-      
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
     }
 
-    setPassword(pass)
-
-  }, [length, numberAllowed, charAllowed, setPassword])
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input 
-          type="text"
-          value={password}
-          className="outline-none w-full py-1 px-3"
-          placeholder="password"
-          readOnly
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
           ></input>
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>length: {length}</label>
         </div>
       </div>
     </>
