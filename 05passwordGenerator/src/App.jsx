40412b8dc530d9baa70b937f7ffb201492af6a83
Harshita Raj <harshitaonweb@gmail.com>
2025-07-21 20:04:04 +0530

    Added Layout

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,15 +1,35 @@
-import { use, useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(False);
-  const [charAllowed, setCharAllowed] = useState(False);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
-      <h1 className="text-4xl text-center">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input type="text"></input>
+        </div>
+      </div>
     </>
   );
 }
