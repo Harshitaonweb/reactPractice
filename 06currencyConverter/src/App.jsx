dcd6210fa2160489970d86d081aa97ccd55dd39b
Harshita Raj <harshitaonweb@gmail.com>
2025-07-24 23:22:55 +0530

    cureency converter

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-      <h1 className='text-3xl bg-blue-500'>Currency App with cookie</h1>
+      <h1 className='text-3xl bg-blue-500 text-white'>Currency App with cookie</h1>
     </>
   )
 }
