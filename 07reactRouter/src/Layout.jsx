32cdcf93285e0b72882285cd2e87713c4f5f11b4
Harshitaonweb <harshitaonweb@gmail.com>
2025-08-19 13:03:42 +0530

    Website made

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function Layout() {
   return (
