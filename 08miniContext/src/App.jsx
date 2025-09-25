36c3c19365bbafa7e4e177ab5e2454a336059e56
Harshitaonweb <harshitaonweb@gmail.com>
2025-09-25 19:54:36 +0530

    UserContext

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,10 +1,11 @@
 import App from "./App.css";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <h1>Cookie</h1>
-    </>
+    </UserContextProvider>
   );
 }
 
