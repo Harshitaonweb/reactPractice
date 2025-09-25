c8c266a5274ca4c5dacee4c071114756ce635b4c
Harshitaonweb <harshitaonweb@gmail.com>
2025-09-25 20:01:11 +0530

    Login page

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -2,7 +2,13 @@ import React, { useState, useContext } from "react";
 import UserContext from "../context/UserContext";
 
 function Login() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+    </div>
+  );
 }
 
 export default Login;
