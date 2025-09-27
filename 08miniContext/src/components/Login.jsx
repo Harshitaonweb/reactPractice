9d56b3723062ae9ae636c9c1c3527351d6cd364a
Harshitaonweb <harshitaonweb@gmail.com>
2025-09-27 22:51:02 +0530

    add

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
   return (
     <div>
       <h2>Login</h2>
-      <input type="text" placeholder="username" />
+      <input type="text" value={username} placeholder="username" />
       <input type="text" placeholder="password" />
       <button onClick={handleSubmit}>Submit</button>
     </div>
