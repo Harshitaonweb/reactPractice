5dc0fef41995cf05b8e71049035a469a2c958ac8
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-07 23:34:54 +0530

    Login page

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -5,6 +5,8 @@ function Profile() {
   const { user } = useContext(UserContext);
 
   if (!user) return <div>Please Login</div>;
+
+  return <div>Welcome {user.username}</div>;
 }
 
 export default Profile;
