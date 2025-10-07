4abf4c02c20aad1f881d102d390048d491f0c15a
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-07 23:31:48 +0530

    add

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import UserContext from "../context/UserContext";
 
 function Profile() {
-  const { setUser } = useContext(UserContext);
+  const { user } = useContext(UserContext);
   return <div></div>;
 }
 
