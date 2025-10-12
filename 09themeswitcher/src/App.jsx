f09dcda222bd4bb6014759aa8cb2987c492695ec
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-13 00:33:37 +0530

    Add

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -5,8 +5,10 @@ function App() {
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+        {/*theme button*/}
 
         <div className="w-full max-w-sm mx-auto"></div>
+        {/*Card*/}
       </div>
     </div>
   );
