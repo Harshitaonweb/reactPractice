6ed230039cbf699fbc5130bc971ac98af9b656fb
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-13 20:42:44 +0530

    Add

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-        {/*theme button*/}
+    <ThemeProvider>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          {/*theme button*/}
 
-        <div className="w-full max-w-sm mx-auto"></div>
-        {/*Card*/}
+          <div className="w-full max-w-sm mx-auto"></div>
+          {/*Card*/}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
