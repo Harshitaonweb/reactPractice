df19c8de05c580c0d553c98c2c70f788453501db
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-16 18:11:28 +0530

    Add

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { ThemeProvider } from "./context/theme";
+import { ThemeProvider } from "./contexts/theme";
 import ThemeBtn from "./components/ThemeBtn";
 import Card from "./components/Card";
 
@@ -12,10 +12,10 @@ function App() {
   };
 
   const darkTheme = () => {
-    setThemeMode("dark ");
+    setThemeMode("dark");
   };
 
-  // for change in theme
+  // actual change in theme
 
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
@@ -23,14 +23,16 @@ function App() {
   }, [themeMode]);
 
   return (
-    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-          <ThemeBtn />
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
-          <div className="w-full max-w-sm mx-auto"></div>
-          <Card />
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
