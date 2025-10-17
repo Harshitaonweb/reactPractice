6ca3637adc410ec61cfb5784b3cae392b41e083b
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-17 23:55:49 +0530

    Add

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { TodoProvider } from "./contexts/Index";
 import "./App.css";
 
 function App() {
-  const [Todos, setTodos] = useState();
+  const [Todos, setTodos] = useState([]);
 
   return (
-    <>
+    <TodoProvider>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -19,7 +18,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   );
 }
 
