c16f7d1a879227a59d95d5deedf09ffd64c75555
Harshitaonweb <harshitaonweb@gmail.com>
2025-10-18 23:44:50 +0530

    Add

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { TodoProvider } from "./contexts/Index";
+import { TodoProvider } from "./contexts";
 import "./App.css";
 
 function App() {
   const [Todos, setTodos] = useState([]);
 
   return (
-    <TodoProvider>
+    <TodoProvider value={{ Todos, addTodos }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
