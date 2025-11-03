627dbf89b60a6b8edb7545b223ce473b55cb5ff8
Harshitaonweb <harshitaonweb@gmail.com>
2025-11-03 22:46:53 +0530

    Add

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form go here */}</div>
+          <div className="mb-4">{/* Todo form will go here */}</div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop , Add TodoItem here */}
           </div>
