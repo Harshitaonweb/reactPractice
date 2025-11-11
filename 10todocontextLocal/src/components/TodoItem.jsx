87a743c9d56e6a7be727476ca50c04c0aa89a142
Harshitaonweb <harshitaonweb@gmail.com>
2025-11-11 22:39:24 +0530

    add

--- 10todocontextLocal/src/components/TodoItem.jsx
+++ 10todocontextLocal/src/components/TodoItem.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useTodo } from "../contexts/TodoContext";
 
 function TodoItem({ todo }) {
+  const {} = useTodo;
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
