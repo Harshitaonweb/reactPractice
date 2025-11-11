ac8d888b75f3e571d786e809117b474035a492cf
Harshitaonweb <harshitaonweb@gmail.com>
2025-11-11 22:36:29 +0530

    add

--- 10todocontextLocal/src/components/TodoForm.jsx
+++ 10todocontextLocal/src/components/TodoForm.jsx
@@ -20,6 +20,8 @@ function TodoForm() {
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => setTodo()}
       />
       <button
         type="submit"
