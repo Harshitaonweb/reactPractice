cd4beb0b540fdbc0588290036d3013369bffd194
Harshitaonweb <harshitaonweb@gmail.com>
2025-11-11 23:00:25 +0530

    add

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts/TodoContext";
 import "./App.css";
+import TodoForm from "./components/Todoform";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -56,9 +57,15 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form will go here */}</div>
+          <div className="mb-4">
+            {/* Todo form will go here */}
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop , Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}></div>
+            ))}
           </div>
         </div>
       </div>
