e28f8b1a48bcb2d643d98c3ebaa5c8969c11ab9d
Harshitaonweb <harshitaonweb@gmail.com>
2026-02-19 20:09:52 +0530

    add

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,5 +1,18 @@
 import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
-export class AuthService {}
-export default AuthService;
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.endpoint)
+      .setProject(conf.projectId)
+      .setDevKey(conf.apikey);
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
